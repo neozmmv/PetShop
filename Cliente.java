be7a9a38e150ec5da368fe572b634eb380612b53
Henrique Luza <henriqueluza@gmail.com>
2025-03-18 15:55:24 -0300

    Adiciona classe Pet e funções básicas

--- Cliente.java
+++ Cliente.java
@@ -1,29 +1,57 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class Cliente {
     private String nome;
     private String telefone;
     private String email;
-
-    // FALTA ADICIONAR A LISTA DE PETS.
+    private List<Pet> pets;
+    //adicionei a lista de pets e importei as bibliotecas pra isso
 
     // Construtor do cliente
     public Cliente(String nome, String telefone, String email) {
         this.nome = nome;
         this.telefone = telefone;
         this.email = email;
+        this.pets = new ArrayList<>();
     }
 
     // Funções de pegar informação (cliente.nome(), cliente.email(), etc...)
-    public String nome() {
+    //adicionei as funções relacionadas à classe pet e ao cliente
+    public void adicionarPet(Pet pet) {
+        pets.add(pet);
+    }
+    
+    public void removerPet(Pet pet) {
+        pets.remove(pet);
+    }
+    
+    public List<Pet> getPets() {
+        return pets;
+    }
+    
+    public String getNome() {
         return nome;
     }
-
-    public String telefone() {
+    
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    public String getTelefone() {
         return telefone;
     }
-
-    public String email() {
+    
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+    
+    public String getEmail() {
         return email;
+    }
+    
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     /*
---
+++ Pet.java
@@ -0,0 +1,65 @@
+public class Pet {
+    private String nome;
+    private String especie;
+    private String raca;
+    private int idade;
+    private double peso;
+    private Cliente dono;
+    
+    public Pet(String nome, String especie, String raca, int idade, double peso, Cliente dono){
+        this.nome = nome;
+        this.especie = especie;
+        this.raca = raca;
+        this.idade = idade;
+        this.peso = peso;
+        this.dono = dono;
+    }
+    //crie a classe pet e as funções básicas
+    public String getNome() {
+        return nome;
+    }
+    
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    public String getEspecie() {
+        return especie;
+    }
+    
+    public void setEspecie(String especie) {
+        this.especie = especie;
+    }
+    
+    public String getRaca() {
+        return raca;
+    }
+    
+    public void setRaca(String raca) {
+        this.raca = raca;
+    }
+    
+    public int getIdade() {
+        return idade;
+    }
+    
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+    
+    public double getPeso() {
+        return peso;
+    }
+    
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+    
+    public Cliente getDono() {
+        return dono;
+    }
+    
+    public void setDono(Cliente dono) {
+        this.dono = dono;
+    }
+}
