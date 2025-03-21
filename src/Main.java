61e73fd6efedadbab9ba6aa00275caefb189ee6f
Enzo Gutiérrez Pereira <enzogp2005@gmail.com>
2025-03-20 22:07:03 -0300

    Comentário sobre função executeQuery

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
         connection.setPassword(fileReader.get_Password());
         connection.mount_Url(fileReader.get_Host(), fileReader.get_Port(), fileReader.get_Database());
         System.out.println(connection.getConnection());
-        connection.executeQuery("SHOW TABLES;", true);
+        connection.executeQuery("SHOW TABLES;", true); // essa funcao retorna um ResultSet (resultado da query)
 
     }
 }
