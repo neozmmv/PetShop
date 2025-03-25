7ddab6af90450df99b6b6d09cdc92821cf1d3f83
Enzo Gutiérrez Pereira <enzogp2005@gmail.com>
2025-03-25 14:49:18 -0300

    Retornos de Int e String no banco.
    Criei funções que retornam Int ou String de acordo com a query, e uma função de update / inserir informações na tabela.

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,50 @@
+import java.sql.*;
+
 public class Main {
     public static void main(String[] args) {
+            //ler o arquivo com as configs do banco
+            FileReader fileReader = new FileReader("server.conf");
+            Connect connection = new Connect();
+            connection.setUser(fileReader.get_User());
+            connection.setPassword(fileReader.get_Password());
+            connection.mount_Url(fileReader.get_Host(), fileReader.get_Port(), fileReader.get_Database());
 
-        FileReader fileReader = new FileReader("server.conf");
-        Connect connection = new Connect();
-        connection.setUser(fileReader.get_User());
-        connection.setPassword(fileReader.get_Password());
-        connection.mount_Url(fileReader.get_Host(), fileReader.get_Port(), fileReader.get_Database());
-        System.out.println(connection.getConnection());
-        connection.executeQuery("SHOW TABLES;", true); // essa funcao retorna um ResultSet (resultado da query)
+            System.out.println(connection.getConnection());
+
+            //apenas para consultas no banco
+            //ResultSet result = connection.executeQuery("SELECT id from tabela2 where nome = 'NOME';", false); //resultado da query
+
+
+
+            //--------------- O QUE IMPORTA É ISSO AQUI, O RESTO É TESTE --------------------------
+            int test = connection.getIntByQuery("SELECT id from tabela2 where nome = 'Joao';"); //Joao só nome para testar
+            System.out.println("VALOR DE TEST:" + test);
+
+            String nome = connection.getStringByQuery("Select nome from tabela2 where id = 1");
+            System.out.println("VALOR DE NOME: " + nome);
+
+            //Adicionar informação no banco:
+            connection.executeUpdate("(COMANDO PARA INSERIR ALGO NO BANCO)", true);
+            //-------------------------------------------------------------------------------------
+
+
+
+
+            //executa a query e retorna a quantidade de linhas afetadas.
+            //int rows = connection.executeUpdate("Insert into tabela2 values(2,'Joao');", true);
+            //QueryResult resultado = new QueryResult(result);
+            //resultado.printQuery("id");
+
+            //MOSTRAR RESULTADOS DA QUERY DE CONSULTA (RESULTSET)
+            //MESMA COISA DE "resultado.printQuery()"
+            /*if (result != null) {
+                while (result.next()) {
+                    System.out.println("Nome: " + result.getString("nome"));
+                }
+                result.close(); // Fechar o ResultSet após o uso
+            }*/
+
+            //System.out.println("Linhas: " + rows);
 
     }
 }
---
+++ src/QueryResult.java
@@ -0,0 +1,20 @@
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class QueryResult {
+    private ResultSet resultSet;
+
+    public QueryResult(ResultSet rs)
+    {
+        this.resultSet = rs;
+    }
+
+    //Função para printar o resultado da query de busca
+    public void printQuery(String label) throws SQLException
+    {
+        while(resultSet.next())
+        {
+            System.out.println(resultSet.getString(label));
+        }
+    }
+}
