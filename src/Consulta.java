388f0785c6d3aba0e5b7bd84a0963fb92791d7ad
Enzo Gutiérrez Pereira <enzogp2005@gmail.com>
2025-04-02 10:10:21 -0300

    Criei a classe Consulta

---
+++ src/Consulta.java
@@ -0,0 +1,39 @@
+import java.time.LocalDateTime;
+
+public class Consulta extends Servico {
+    private String veterinario;
+    private boolean consultaUrgente;
+
+    public Consulta(String descricao, double preco, LocalDateTime dataAgendamento, Cliente cliente, Pet pet, String veterinario, boolean consultaUrgente) {
+        super(descricao, preco, dataAgendamento, cliente, pet);
+        this.veterinario = veterinario;
+        this.consultaUrgente = consultaUrgente;
+    }
+
+    @Override
+    public double calcularPreco() {
+        //mais 20% no preço se for urgente
+        return consultaUrgente ? getPreco() * 1.2 : getPreco();
+    }
+
+    public String getVeterinario() {
+        return veterinario;
+    }
+
+    public void setVeterinario(String veterinario) {
+        this.veterinario = veterinario;
+    }
+
+    public boolean isConsultaUrgente() {
+        return consultaUrgente;
+    }
+
+    public void setConsultaUrgente(boolean consultaUrgente) {
+        this.consultaUrgente = consultaUrgente;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Veterinário: " + veterinario + ", Urgente: " + (consultaUrgente ? "Sim" : "Não");
+    }
+}
